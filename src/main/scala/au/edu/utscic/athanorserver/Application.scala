3bae2563407e1455200e2cf31b29bf5481e3d0c8
Joseph Harfouch <joseph@harfouch.id.au>
2017-10-26 15:14:44 +0800

    Support for tailoring of grammar parser path.
    Enable support for trying both a local and a docker grammar parser files,
    and for tailoring the grammar path via property files and environment values.
    See the accompanying documentation changes for further explanation of
    these changes.

--- src/main/scala/au/edu/utscic/athanorserver/Application.scala
+++ src/main/scala/au/edu/utscic/athanorserver/Application.scala
@@ -1,5 +1,7 @@
 package au.edu.utscic.athanorserver
 
+import au.edu.utscic.athanorserver.StreamsContext.log
+import au.edu.utscic.athanorserver.athanor.Athanor
 import au.edu.utscic.athanorserver.server.Server
 
 /** The Application entry point starts the akka-http server
@@ -8,5 +10,14 @@ import au.edu.utscic.athanorserver.server.Server
   *
   */
 object Application extends App {
-  Server.startServer("0.0.0.0",8083)
+
+  log.info("Checking grammar path")
+  val parserLoaded = Athanor.isGrammarParserLoaded()
+  if (parserLoaded == false) {
+    log.error("Error: Failed to load Grammar Parser")
+    log.info("Please See instructions on tailoring the path to the Grammar Parser")
+  }
+  else {
+    Server.startServer("0.0.0.0", 8083)
+  }
 }
